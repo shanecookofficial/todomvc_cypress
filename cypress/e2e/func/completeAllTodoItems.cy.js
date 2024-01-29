31a13d2c1b7318bea7bf51a64b2b91dcf10e2be8
shanecookofficial <shanecookofficial@gmail.com>
2024-01-29 09:05:46 -0700

    Created completeAllTodoItems functionality test

---
+++ cypress/e2e/func/completeAllTodoItems.cy.js
@@ -0,0 +1,24 @@
+describe('Complete All Todo Items', () => {
+  it('All Todo Items are completed', () => {
+    cy.fixture('general.json').then((data) => {
+      cy.visit(data.todoSite);
+      const todoName = "Test"
+
+      // Create several Todo Items
+      for (let i = 1; i < 4; i++) {
+
+        cy.get('[data-testid="text-input"]').click().type(todoName + i).type('{enter}');
+      }
+
+      // Click down arrow to complete all items
+      cy.get('[data-testid="toggle-all"]').click()
+
+      // Verify all todo list items are completed
+      cy.get('.todo-list').find('li').each(($li, index) => {
+
+        expect($li).to.have.class('completed');
+      });
+
+    })
+  })
+})
