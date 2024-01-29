a688dd08039b3365be74a6a169bdd6995de1a224
shanecookofficial <shanecookofficial@gmail.com>
2024-01-29 09:51:33 -0700

    Added github workflows CI/CD yml and updated config

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  projectId: 'qzxgex',
+  projectId: 'fhyhny',
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
