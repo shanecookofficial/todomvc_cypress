4f92b8886d2bc11e605e7efa8e13937641d9d05a
shanecookofficial <shanecookofficial@gmail.com>
2024-01-29 08:50:12 -0700

    Created createTodoItem functional test

---
+++ cypress/e2e/func/createTodoItem.cy.js
@@ -0,0 +1,25 @@
+describe('Create a Todo Item', () => {
+  it('A Todo Item was created', () => {
+    cy.fixture('general.json').then((data) => {
+      cy.visit(data.todoSite);
+      const todoName = "Test 1"
+
+      // Click on the "What needs to be done" bar
+
+      // Type a todo item name
+
+      // Press enter
+      cy.get('[data-testid="text-input"]').click().type(todoName).type('{enter}');
+
+
+      // Verify that a new item has been created
+
+      // There should only be 1 li created
+      cy.get('[data-testid="todo-list"]').find('li').should('have.length', 1);
+
+      // The li should contain the text stored in the todoName variable
+      cy.get('.view').first().find('[data-testid="todo-item-label"]').should('have.text', todoName);
+
+    })
+  })
+})
